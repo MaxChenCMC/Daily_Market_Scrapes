b016d2364ca8aa4b98061734a309310d7867cdbf
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-04-22 00:07:23 +0800

    Update PreMarketNotify.py

--- PreMarketNotify.py
+++ PreMarketNotify.py
@@ -1,53 +1,59 @@
 import requests, pandas as pd
-from io import StringIO
-from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+import plotly.graph_objs as go
 from NotifyBase import NotifyBase
+font_family = "Arial, sans-serif, 微軟正黑體, Microsoft JhengHei, 黑體-简, SimHei"
 
 
-def MopsYYYMMDD(audience: str):
+def RankTradingVolTWD(audience: str):
     '''
-    '30 0 * * 1-5'
-    不能選"_q1"會爬不到，過12想爬只能手動寫死日期
+    histock不同排行有對應網址很貼心、若要加回'價格▼'則read_html就要補且lambda也要補
     '''
-    # Monday = 0, Friday = 4
-    if datetime.now().weekday() == 5: YYYMMDD = datetime.now() - timedelta(days = 1)
-    elif datetime.now().weekday() == 6: YYYMMDD = datetime.now() - timedelta(days = 2)
-    else: YYYMMDD = datetime.now()
-    url = f"https://mops.twse.com.tw/mops/web/t56sb12?encodeURIComponent=1&run=&step=2&year={str(YYYMMDD.year-1911)}&month={YYYMMDD.strftime('%m')}&day={YYYMMDD.strftime('%d')}&report=SY&firstin=true"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    headers = { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
-    response = requests.post(url, headers = headers)
-    dfs = pd.read_html(StringIO(response.text))
-    df = pd.concat([_ for _ in dfs if not _.empty])
-    df = df[[('公司名稱', '公司名稱'),('申報人身分', '申報人身分'),('目前持有股數', '自有持股'),('預定轉讓總股數', '自有持股')]]
-    df.columns = ["公司","申報身份","現有","預定轉讓"]
+    df = pd.read_html("https://histock.tw/stock/rank.aspx?m=13&d=1&p=all")[0][['代號▼','名稱▼','漲跌幅▼','振幅▼','成交值(億)▼']].head(15)
+    df = df.replace("--", "+0.0%")
+    df = df[df["代號▼"].apply(lambda x: len(x) == 4)]
+    df["漲跌幅▼"] = df["漲跌幅▼"].apply(lambda x: float(x[:-1]))
+    df["振幅▼"] = df["振幅▼"].apply(lambda x: float(x[:-1]))
+    df["成交值(億)▼"] = df["成交值(億)▼"].apply(lambda x: int(x))
+    df.columns = ["股號", "股名", "漲幅", "振幅", "成交值"]
+    VolaPct, VolTWD = 7.0, 20.0
+    res = df[(df["振幅"] > VolaPct) & (df["成交值"] > VolTWD)].head(10) # & (df["漲幅"] > chgPct) 
 
-    title = "▌股權轉讓事前申報"
-    url = "https://mops.twse.com.tw/mops/web/t56sb12_q1"
+    title = "▌振幅>7%且成交值>20億的人氣股"
+    url1 = "https://histock.tw/stock/rank.aspx?m=13&d=1&p=all"
     msg = ["\n" + title]
-    [msg.append(i + "\n") for i in [url, df.dropna().to_string(index = False, col_space = 8)]]
-    NotifyBase(audience, "文", "\n".join(msg))
-    
-    
-def AnnouncementNoticeYYYYMMDD(audience: str):
-    '''
-    '30 0 * * 1-5'
-    '''
-    if datetime.now().weekday() == 5: YYYYMMDD = (datetime.now() - timedelta(days = 1)).strftime("%Y%m%d")
-    elif datetime.now().weekday() == 6: YYYYMMDD = (datetime.now() - timedelta(days = 2)).strftime("%Y%m%d")
-    else: YYYYMMDD = datetime.now().strftime("%Y%m%d")
-    response = requests.get(f"https://www.twse.com.tw/rwd/zh/announcement/notice?querytype=1&stockNo=&selectType=&startDate={YYYYMMDD}&endDate={YYYYMMDD}&sortKind=STKNO&response=json").json()["data"]
-    [[response[i][2],response[i][4]+"\n\n"] for i in range(len(response))]
-    df = pd.DataFrame([[response[i][2],response[i][4][:35]] for i in range(len(response))], columns=['Company', 'Details']).head(10)
-    
-    title = "▌注意有價證券"
-    url = "https://www.twse.com.tw/zh/announcement/notice.html"
-    msg = ["\n" + title]
-    [msg.append(i + "\n") for i in [url, df.to_string(index = True)]]
-    NotifyBase(audience, "文", "\n".join(msg))
+    [msg.append(i + "\n") for i in [url1, res.to_string(index = False, col_space = 8)]]
+    NotifyBase(audience, "文", "\n\t".join(msg))
 
 
-MopsYYYMMDD("0")
-AnnouncementNoticeYYYYMMDD("0")
+def Candlestick(audience: str):
+    df = pd.read_html("https://histock.tw/stock/rank.aspx?m=13&d=1&p=all")[0][['代號▼']]
+    df = df.replace("--", "+0.0%")
+    df = df[df["代號▼"].apply(lambda x: len(x) == 4)].head(15)
+    tickers = df["代號▼"].tolist()
+    part_url = "|".join(["tse_" + ticker + ".tw" for ticker in tickers] + ["otc_" + ticker + ".tw" for ticker in tickers])
+    url = f"https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch={part_url}"
+    Datas = requests.get(url).json()["msgArray"]
+
+    _temp = []
+    for Data in range(len(Datas)):
+        # ["股名"] 拼接 [開o→高h→底l→收z 減昨收y的幅度]
+        Fields = [Datas[Data][Field] for Field in ["n","o","h","l","z","y"]]
+        Fields_ = [Fields[0]] + [
+            round(((float(x) - float(Fields[-1]))*100 / float(Fields[-1])), 2)
+            for x in Fields[1:-1]]
+        _temp.append(Fields_)
+    df = pd.DataFrame(_temp, columns=["name", "open", "high", "low", "close"])
+    fig = go.Figure(
+        data = [go.Candlestick(x = df['name'],
+                               open = df['open'], high = df['high'], low = df['low'], close = df['close'],
+                               increasing_line_color = "red", decreasing_line_color = "green"
+                               )
+                ], layout = go.Layout(xaxis_rangeslider_visible = False))
+    fig.update_layout(title='▌成交值前15大的焦點股日K走勢強弱比較', yaxis_title = 'Change%', font=dict(family=font_family))
+    #, plot_bgcolor = 'black', xaxis_title='Name', yaxis = dict(title='Price', tickformat='.2f')
+    fig.write_image("RanksCandlestick.png")
+    NotifyBase(audience, "圖", "RanksCandlestick.png")    
+
+
+RankTradingVolTWD("1")
+Candlestick("1")
