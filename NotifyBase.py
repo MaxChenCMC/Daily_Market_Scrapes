7f7a2661a25e7870bcf8ffbb246c9b6027604cbd
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-04-21 11:43:01 +0800

    Update NotifyBase.py

--- NotifyBase.py
+++ NotifyBase.py
@@ -1,14 +1,16 @@
 import requests
 
-def NotifyBase(acct="1", msg: str, arg):
+def NotifyBase(acct: str, msg: str, arg):
     '''
     08:30
     15:10
     16:30
     '''
-    if acct == "0": token = 'Ww9Y7PSHCNkdmdGkxpdPT54vMGf0VaZBoMZH7BudlVS'    # 台股數據通知中心
     elif acct == "1": token = "TVR9kcFsdDS0Si7YtusO4bl3gcgUUjKz248yXivtJ56"  # 個人開發😎先選【透過1 對1】
-    elif acct == "2": token = "3WKXlzNj7DixgamCXtvdkX9GRsbZv5XvepjnlWWdeYz"  # 期交所測試階段先一對多
+
+    # if acct == "0": token = 'Ww9Y7PSHCNkdmdGkxpdPT54vMGf0VaZBoMZH7BudlVS'    # 台股數據通知中心
+    # elif acct == "1": token = "TVR9kcFsdDS0Si7YtusO4bl3gcgUUjKz248yXivtJ56"  # 個人開發😎先選【透過1 對1】
+    # elif acct == "2": token = "3WKXlzNj7DixgamCXtvdkX9GRsbZv5XvepjnlWWdeYz"  # 期交所測試階段先一對多
     url = 'https://notify-api.line.me/api/notify'
     headers = {'Authorization': 'Bearer ' + token}
     if msg == "圖":
