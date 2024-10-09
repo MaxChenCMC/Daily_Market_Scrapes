53ab4dac901851ab30bcda6c9f4dbd60d3977305
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-10-09 17:12:09 +0800

    Update getQuoteListOption.py

--- getQuoteListOption.py
+++ getQuoteListOption.py
@@ -1,68 +1,59 @@
 import requests, pandas as pd
 from datetime import datetime, timedelta
 
-DispEName_range_call = []
-DispEName_range_put = []
 
-def step1(mkt_type: str) -> pd.DataFrame:
+def strike_range_code(mkt_type: str, weekth: str):
+    # network找getQuoteDetail
     url = "https://mis.taifex.com.tw/futures/api/getQuoteDetail"
-    payload, payload_all = dict(), dict()
-    if mkt_type == "0":
-        # ★這裡要週更
-        payload = {"SymbolID":["TXF-S","TXFJ4-M","TXO-R"]}
-        payload_all = {"MarketType":"0","SymbolType":"O","KindID":"1","CID":"TXO","ExpireMonth":"202410W2", "RowSize":"全部","PageNo":"","SortColumn":"","AscDesc":"A"}
-    elif mkt_type == "1":
-        # ★這裡要週更
-        payload =  {"SymbolID":["TXF-S","TXFJ4-M","TXO-R"]}
-        payload_all = {"MarketType":"1","SymbolType":"O","KindID":"1","CID":"TXO","ExpireMonth":"202410W2", "RowSize":"全部","PageNo":"","SortColumn":"","AscDesc":"A"}
-
-    res = requests.post(url, json = payload).json()["RtData"]['QuoteList'][1]
-    last_close = float(res['CLastPrice'])
-    # print(last_close)
-    close_to_strike = int(last_close / 50) * 50
-    stike_range = [(close_to_strike + i * 50) for i in range(-2,3)] #range(-3,4)
-
-    for i in stike_range:
-        # ★這裡要週更 
-        DispEName_range_call.append(f"TX2W2104;{i}C")
-        DispEName_range_put.append( f"TX2W2104;{i}P")
+    payload = {"SymbolID":["TXF-S","TXFJ4-M","TXO-R"]}
+    if (datetime.now() + timedelta(hours = 8)).hour >= 15:
+        try:
+            res = requests.post(url, json = payload).json()["RtData"]['QuoteList'][1]
+            last_close = float(res['CLastPrice'])
+            close_to_strike = int(last_close / 50) * 50
+            stike_range = [(close_to_strike + i * 50) for i in range(-1,2)] # 暫時只取±1個履約價   更多就range(-2,3)或range(-3,4)
+            stike_range_code = []
+            for k in [(close_to_strike + i * 50) for i in range(-1,2)]:
+                stike_range_code.append(f'{weekth}104;{k}C')
+                stike_range_code.append(f'{weekth}104;{k}P')
+            return stike_range_code
+        except Exception as e:
+            print(e)
 
 
-    url = "https://mis.taifex.com.tw/futures/api/getQuoteListOption"
-    res = requests.post(url, json = payload_all).json()["RtData"]['QuoteList']
-    test = []
-    for i in range(len(res)):
-        if res[i]['DispEName'] in DispEName_range_call:
-            test.append([
-                # res[i]['CTime'],
-                res[i]['DispEName'][-6:],
-                res[i]['CBestBidPrice'],
-                res[i]['CLastPrice'],
-                res[i]['CBestAskPrice']
+def strike_range_df(src):
+    mkt_tpye = "1"
+    weekth = "TXO"
+    month = "10"
+    url =   "https://mis.taifex.com.tw/futures/api/getQuoteListOption"
+    payload =  {"MarketType": mkt_tpye, "SymbolType":"O","KindID":"1","CID": weekth, "ExpireMonth": f"2024{month}","RowSize":"全部","PageNo":"","SortColumn":"","AscDesc":"A"}
+    quote_table = requests.post(url, json = payload).json()["RtData"]['QuoteList']
+    _temp = []
+    for i in range(len(quote_table)):
+        if quote_table[i]['DispEName'] in src:
+            _temp.append([
+                # 'time', 'strike', 'mid_price', 'spread'
+                (datetime.now()+ timedelta(hours = 8)).strftime("%Y-%m-%d %H:%M:%S"),
+                quote_table[i]['DispEName'][-6:-1],
+                (float(quote_table[i]['CBestAskPrice']) + float(quote_table[i]['CBestBidPrice'])) / 2,
+                float(quote_table[i]['CBestAskPrice']) - float(quote_table[i]['CBestBidPrice']) 
             ])
-        elif res[i]['DispEName'] in DispEName_range_put:
-            test.append([
-                # res[i]['CTime'],
-                res[i]['DispEName'][-6:],
-                res[i]['CBestBidPrice'],
-                res[i]['CLastPrice'],
-                res[i]['CBestAskPrice']
-            ])
-    # return res  "Time", 
-    df = pd.DataFrame(test, columns = ["Stike", "Bid", "Close", "Ask"])
-    df[["Bid", "Close", "Ask"]] = df[["Bid", "Close", "Ask"]].astype(float)
 
-    ts = (datetime.now()+ timedelta(hours = 8)).strftime('%H:%M:%M')
-    ret = [
-        [df[['Close']][0:2].sum().values[0], df[['Ask']][0:2].sum().values[0] - df[['Bid']][0:2].sum().values[0]],
-        [df[['Close']][2:4].sum().values[0], df[['Ask']][2:4].sum().values[0] - df[['Bid']][2:4].sum().values[0]],
-        [df[['Close']][4:6].sum().values[0], df[['Ask']][2:3].sum().values[0] - df[['Bid']][4:6].sum().values[0]],
-        [df[['Close']][6:8].sum().values[0], df[['Ask']][2:3].sum().values[0] - df[['Bid']][6:8].sum().values[0]],
-        [df[['Close']][8:10].sum().values[0], df[['Ask']][2:3].sum().values[0] - df[['Bid']][8:10].sum().values[0]],
-    ]
-    return df #, pd.DataFrame(ret)
+    _df = pd.DataFrame(_temp, columns=['time', 'strike', 'mid_price', 'spread'])
+    _df['time'] = pd.to_datetime(_df['time'])
+    _df['strike'] = _df['strike'].astype(str)
+    _df['mid_price'] = pd.to_numeric(_df['mid_price'])
+    _df['spread'] = pd.to_numeric(_df['spread'])
+    # Group by 'time' and 'strike', then sum 'parity_sum' and 'spread_sum'
+    grouped_df = _df.groupby(['time', 'strike']).agg({
+        'mid_price': 'sum',
+        'spread': 'sum'
+    }).reset_index()
 
-arg = step1("1").to_string(index = False)
+    return grouped_df.sort_values(['time', 'strike'])
+
+
+arg = strike_range_df(strike_range_code("1", "TXO")).to_string(index = False)
 token = 'Ww9Y7PSHCNkdmdGkxpdPT54vMGf0VaZBoMZH7BudlVS'
 url = 'https://notify-api.line.me/api/notify'
 headers = {'Authorization': 'Bearer ' + token} 
