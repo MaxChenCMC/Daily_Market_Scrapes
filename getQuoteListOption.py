a9242f79caf10f366a237191df916c7f3bc5f342
諶俊銘 <58615261+MaxChenCMC@users.noreply.github.com>
2024-12-18 17:25:40 +0800

    Update getQuoteListOption.py
    new version

--- getQuoteListOption.py
+++ getQuoteListOption.py
@@ -1,71 +1,24 @@
 import requests, pandas as pd
 from datetime import datetime, timedelta
 
-def strike_range_code(DispEName: str, mkt_type: str, contract_id: str, ExpireMonth: str) -> pd.DataFrame:
-    '''
-    RegularSession與AfterHoursSession，取最新成交價的"Request URL"都一樣，payload也一樣，只差在MarketType是0或1
-    先產±1個履約價的CP共6個CODE，
-    A is 1, K is 11
-    "TXF-S", "TXFK4-F", "TXO-Q"
-    "TXF-S", "TXFK4-M", "TXO-R"
-    '''
-    try:
-        # change per week contract
-        if mkt_type == "0":
-            arg1: str = "M"
-            arg2: str = "R"
-        elif mkt_type == "1":
-            arg1: str = "M"
-            arg2: str = "R"
-            
-        res = requests.post("https://mis.taifex.com.tw/futures/api/getQuoteDetail",
-                            json={"SymbolID": [
-                                # type: ignore
-                                # type: ignore
-                                "TXF-S", f"TXFK4-{arg1}", f"TXO-{arg2}"]}
-                            ).json()["RtData"]['QuoteList'][1]
-        QRTime = datetime.strptime(res["CTime"], '%H%M%S').strftime('%H:%M:%S')
-        last_close = float(res['CLastPrice'])
-        close_to_strike = int(last_close / 50) * 50
-        stike_range_code = []
-        for k in [(close_to_strike + i * 50) for i in range(-1,2)]:
-            stike_range_code.append(f'{DispEName + str(k)}C')
-            stike_range_code.append(f'{DispEName + str(k)}P')
-        
-        quote_table = requests.post("https://mis.taifex.com.tw/futures/api/getQuoteListOption", 
-                                    json = {"MarketType": mkt_type,
-                                            "CID": contract_id,
-                                            "ExpireMonth": ExpireMonth, 
-                                            "SymbolType":"O","KindID":"1","RowSize":"全部","PageNo":"","SortColumn":"","AscDesc":"A"},
-                                    ).json()["RtData"]['QuoteList']
-        
-        _temp = []
-        for i in range(len(quote_table)):
-            if quote_table[i]['DispEName'] in stike_range_code:
-                _temp.append([
-                    QRTime,
-                    quote_table[i]['DispEName'][-6:-1],
-                    (float(quote_table[i]['CBestAskPrice']) + float(quote_table[i]['CBestBidPrice'])) / 2,
-                    float(quote_table[i]['CBestAskPrice']) - float(quote_table[i]['CBestBidPrice'])
-                ])
+arg1 = "TXFA5-M" # Network裡getQuoteDetail的paylaod第二個值(第三個值好像也要)
+res = requests.post("https://mis.taifex.com.tw/futures/api/getQuoteDetail", json={"SymbolID": [ "TXF-S", arg1, "TXO-R"]}).json()["RtData"]['QuoteList'][1]
+last_close = float(res['CLastPrice'])
+close_to_strike = int(last_close / 50) * 50
 
-        _df = pd.DataFrame(_temp, columns=['time', 'strike', 'mid_price', 'spread'])
-        _df['strike'] = _df['strike'].astype(str)
-        _df['mid_price'] = pd.to_numeric(_df['mid_price'])
-        _df['spread'] = pd.to_numeric(_df['spread'])
-        grouped_df = _df.groupby(['time', 'strike']).agg({
-            'mid_price': 'sum',
-            'spread': 'sum'
-        }).reset_index()
-        
-        return grouped_df.sort_values(['time', 'strike'])             
+arg2 = "202412W4" # Network裡getQuoteListOption的paylaod的ExpireMonth
+quote_table = requests.post("https://mis.taifex.com.tw/futures/api/getQuoteListOption",
+                            json = {"MarketType": "1", "CID": "TXO", "ExpireMonth": arg2, "SymbolType": "O", "KindID": "1", "RowSize": "全部", "PageNo": "", "SortColumn": "", "AscDesc": "A"},
+                            ).json()["RtData"]['QuoteList']
 
-    except Exception as e:
-        print(e)
+QRTime = datetime.strptime(res["CTime"], '%H%M%S').strftime('%H:%M:%S')
+data = [{QRTime : [item['DispEName'][9:15], (float(item['CBestAskPrice']) + float(item['CBestBidPrice'])) / 2, float(item['CBestAskPrice']) - float(item['CBestBidPrice'])]}
+        for item in quote_table 
+        if item['StrikePrice'] in [str(close_to_strike + i * 50) for i in range(-1,2)]]
 
 
-arg = strike_range_code("TXO114;", "1", "TXO", "202411").to_string(index = False)
+# arg = strike_range_code("TXO114;", "1", "TXO", "202411").to_string(index = False)
 token = 'Ww9Y7PSHCNkdmdGkxpdPT54vMGf0VaZBoMZH7BudlVS'
 url = 'https://notify-api.line.me/api/notify'
 headers = {'Authorization': 'Bearer ' + token} 
-requests.post(url, headers = headers,  data={'message': "\n" + arg})
+requests.post(url, headers = headers,  data={'message': data})
